26483694748bcd29dca00e8560a6f8db67519302
vinaya krishna <vinayakrishna@vinayas-MacBook-Pro.local>
2026-07-14 22:18:21 -0400

    background jobs inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,16 @@
 "use client";
 
-import { Button } from '@/components/ui/button';
 import React from 'react'
 
-import { Authenticated, Unauthenticated, useConvexAuth, useMutation, useQuery } from 'convex/react';
-import { api } from '../../convex/_generated/api';
+import { Authenticated, Unauthenticated} from 'convex/react';
+import Chat from '@/components/Chat';
 
 const Home = () => {
-  const { isAuthenticated } = useConvexAuth();
-  const projects = useQuery(api.projects.get, isAuthenticated ? {} : "skip");
-  const createProject = useMutation(api.projects.create);
 
   return (
     <div>
       <Authenticated>
-        <Button onClick={() => { createProject({ name: "New Project" }) }}>Add New</Button>
-
-        <ul>
-          {projects?.map((project) => (
-            <li key={project._id}>{project.name} : {project.ownerId}</li>
-          ))}
-        </ul>
+        <Chat />
       </Authenticated>
       <Unauthenticated>
         <p>Please sign in to create projects.</p>
