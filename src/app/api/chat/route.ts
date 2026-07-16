3baf86061a510615f8c58869786cefd36835d1ce
vinaya krishna <vinayakrishna@vinayas-MacBook-Pro.local>
2026-07-15 22:45:16 -0400

    Firecrawl integration

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,44 +1,14 @@
-import { streamText, UIMessage, convertToModelMessages, tool, stepCountIs } from 'ai';
-import { anthropic } from "@ai-sdk/anthropic";
-import z from 'zod';
+import { UIMessage } from 'ai';
+import { inngest } from '@/inngest/client';
 
 export async function POST(req: Request) {
-    const { messages }: { messages: UIMessage[] } = await req.json();
-
-    const result = streamText({
-        model: anthropic("claude-haiku-4-5"),
-        messages: await convertToModelMessages(messages),
-        stopWhen: stepCountIs(5),
-        tools: {
-            weather: tool({
-                description: 'Get the weather in a location (in Fareheit)',
-                inputSchema: z.object({
-                    location: z.string().describe('The location to get the weather for')
-                }),
-                execute: async ({ location }) => {
-                    const temperature = Math.round(Math.random() * (90 - 32) + 32);
-                    return {
-                        temperature, location
-                    };
-                }
-            }),
-            convertFahrenheitToCelsius: tool({
-                description: 'Convert a temperature in fahrenheit to celsius',
-                inputSchema: z.object({
-                    temperature: z
-                        .number()
-                        .describe('The temperature in fahrenheit to convert'),
-                }),
-                execute: async ({ temperature }) => {
-                    const celsius = Math.round((temperature - 32) * (5 / 9));
-                    return {
-                        celsius,
-                    };
-                },
-            }),
-        }
-    });
+  const { messages }: { messages: UIMessage[] } = await req.json();
 
 
-    return result.toUIMessageStreamResponse();
+  await inngest.send({
+    name: 'demo/generate',
+    data: { messages },
+  });
+
+  return Response.json({ status : 'started job'});
 }
